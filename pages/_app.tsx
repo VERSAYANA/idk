defaf2cfe6cf8b183af21f01164e13fae846bc49
Ramtin Khalatbari <VERSAYANA@outlook.com>
2021-03-12 19:48:25 +0330

    Add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
