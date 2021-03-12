9952d87da9b3b267b357e08bf89c725e2b01ba76
Ramtin Khalatbari <VERSAYANA@outlook.com>
2021-03-12 21:00:49 +0330

    Make app full screen

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,6 +2,7 @@
 
 // 1. import `extendTheme` function
 import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+import { Breakpoints, Styles } from "@chakra-ui/theme-tools";
 
 // 2. Add your color mode config
 const config: ThemeConfig = {
@@ -9,7 +10,17 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 }
 
+const styles: Styles = {
+    global: {
+        "html, body, #__next": {
+            height: '100%'
+        }
+    }
+}
+
+
+
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, styles })
 
 export default theme
